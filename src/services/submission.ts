a23d7f6f01b603f1843c20278d942d5bba13f421
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-12-26 02:20:12 +0100

    omho

--- src/services/submission.ts
+++ src/services/submission.ts
@@ -1,50 +1,57 @@
-import client from '../utils/axios';
+import client from "../utils/axios"
 
 const SubmissionService = {
-    async submit(submission: { items: string[], images: string[], points: number }) {
-        try {
-            const response = await client().post('/api/submissions/submit', submission);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
+  async submit(submission: {
+    items: string[]
+    images: string[]
+    points: number
+  }) {
+    try {
+      const response = await client().post(
+        "/api/submissions/submit",
+        submission
+      )
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  },
 
-    async getAll() {
-        try {
-            const response = await client().get('/api/submissions/all');
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
+  async getAll() {
+    try {
+      const response = await client().get("/api/submissions/all")
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  },
 
-    async approve(id: string) {
-        try {
-            const response = await client().put(`/api/admin/approve/${id}`);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
+  async approve(id: string) {
+    try {
+      const response = await client().put(`/admin/approve/${id}`)
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  },
 
-    async reject(id: string) {
-        try {
-            const response = await client().put(`/api/admin/reject/${id}`);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
+  async reject(id: string) {
+    try {
+      const response = await client().put(`/admin/reject/${id}`)
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  },
 
-    async getUserSubmissions(userId: string) {
-        try {
-            const response = await client().get(`/api/submissions/user/${userId}`);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    },
-};
+  async getUserSubmissions(userId: string) {
+    try {
+      const response = await client().get(`/api/submissions/user/${userId}`)
+      return response.data
+    } catch (error) {
+      throw error
+    }
+  },
+}
 
-export default SubmissionService;
+export default SubmissionService
